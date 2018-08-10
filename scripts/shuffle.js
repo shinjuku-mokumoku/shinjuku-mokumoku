af44e183a4403ab7dc1655f9c703d5ab6b635fe8
kaeruko <mail@cloxs.jp>
2018-08-10 18:19:03 +0900

    modefied shuffle script as to read file

--- scripts/shuffle.js
+++ scripts/shuffle.js
@@ -1,15 +1,20 @@
-var members = []
-for(var i = 2;i < process.argv.length; i++){
-  members.push(process.argv[i])
-}
+var fs = require('fs');
+var textfile = __dirname+"/members.txt"
+try {
+    containts = fs.readFileSync(textfile, 'utf8');
+    members = containts.split(/\r\n|\r|\n/);
 
-for(var i = members.length - 1; i > 0; i--){
-    var r = Math.floor(Math.random() * (i + 1));
-    var tmp = members[i];
-    members[i] = members[r];
-    members[r] = tmp;
-}
+    for(var i = members.length - 1; i > 0; i--){
+        var r = Math.floor(Math.random() * (i + 1));
+        var tmp = members[i];
+        members[i] = members[r];
+        members[r] = tmp;
+    }
 
-for(var i = 0;i < members.length; i++){
-    console.log("No."+ (i+1) + " : " +members[i] + "")
+    for(var i = 0;i < members.length; i++){
+        console.log("No."+ (i+1) + " : " +members[i] + "")
+    }
+
+} catch(err) {
+    console.log("couldn't open "+textfile )
 }
