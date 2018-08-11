65f9f1da52d66874a03670d430c15b66b523b748
kaeruko <mail@cloxs.jp>
2018-08-11 12:14:33 +0900

    modefy shuffle.js as to get mdfiles from dir

--- scripts/shuffle.js
+++ scripts/shuffle.js
@@ -1,20 +1,30 @@
 var fs = require('fs');
-var textfile = __dirname+"/members.txt"
-try {
-    containts = fs.readFileSync(textfile, 'utf8');
-    members = containts.split(/\r\n|\r|\n/);
 
-    for(var i = members.length - 1; i > 0; i--){
-        var r = Math.floor(Math.random() * (i + 1));
-        var tmp = members[i];
-        members[i] = members[r];
-        members[r] = tmp;
-    }
+fs.readdir(__dirname+'/../meetups/', function(err, meetupsdir){
+    if (err) throw err;
+    meetupsdir.sort(
+        function(a,b){
+        if( parseInt(a) > parseInt(b) ) return -1;
+        if( parseInt(a) < parseInt(b) ) return 1;
+        return 0;
+    });
+    var latest = meetupsdir[0]
+    fs.readdir(__dirname+'/../meetups/'+latest, function(err, mdfiles){
+      if (err) throw err;
 
-    for(var i = 0;i < members.length; i++){
-        console.log("No."+ (i+1) + " : " +members[i] + "")
-    }
+        var members = mdfiles.filter(function(file){
+            return /.*\.md$/.test(file) && !/template\.md$/.test(file) && !/PITCHME\.md$/.test(file) && !/README\.md$/.test(file) && !/kpt\.md$/.test(file)
+        })
+        for(var i = members.length - 1; i > 0; i--){
+            var r = Math.floor(Math.random() * (i + 1));
+            var tmp = members[i];
+            members[i] = members[r];
+            members[r] = tmp;
+        }
 
-} catch(err) {
-    console.log("couldn't open "+textfile )
-}
+        for(var i = 0;i < members.length; i++){
+            console.log("No."+ (i+1) + " : " +members[i] + "")
+        }
+
+    });
+});
