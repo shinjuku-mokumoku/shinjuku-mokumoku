c7d6dcf479079fbf6ed65f7e2dafda84ac51a057
threetreeslight <ae06710@gmail.com>
2018-08-17 13:10:02 +0900

    simplify shuffle scripts

---
+++ scripts/presenter.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var meetup_no = process.argv[2];
+
+console.log(`Meetup #${meetup_no}\n`)
+
+fs.readdir( `${__dirname}/../meetups/${meetup_no}`, (err, files) => {
+  files
+    .filter( (file) => { return !/(README|PITCHME|template|kpt)\.md/.test(file) })
+    .sort((a, b) => {return 0.5 - Math.random()})
+    .forEach((i) => { console.log(i) });
+});
