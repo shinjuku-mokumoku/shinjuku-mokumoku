60bf4c90ada0cb0fd37677492576889035e7e5de
threetreeslight <ae06710@gmail.com>
2019-07-20 19:27:50 +0900

    use single quotes for string

--- functions/deploy_functions.js
+++ functions/deploy_functions.js
@@ -30,7 +30,7 @@ const hasFunctionDiff = async () => {
 
   const res = await octokit.pulls.listFiles({ owner, repo, pull_number: num });
   const functionFileNames = res.data.map(files => files.filename).filter(name => /functions/.test(name));
-  logger.debug(`Change Files: ${functionFileNames.join(",")}`);
+  logger.debug(`Change Files: ${functionFileNames.join(',')}`);
 
   return functionFileNames.length > 0;
 };
