00cdd20553113403f94b0b77b4153ab5d4703383
threetreeslight <ae06710@gmail.com>
2019-09-08 15:46:47 +0900

    add prettier and update node version to 10 on scripts

--- scripts/event_dir_creator.js
+++ scripts/event_dir_creator.js
@@ -1,19 +1,20 @@
-const axios = require('axios');
-const fs = require('fs');
-const { execSync } = require('child_process');
+const axios = require("axios");
+const fs = require("fs");
+const { execSync } = require("child_process");
 
 const logger = console;
 
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
 
-const user = 'shinjuku-mokumoku';
-const repo = 'shinjuku-mokumoku';
+const user = "shinjuku-mokumoku";
+const repo = "shinjuku-mokumoku";
 
 const EventDirCreator = {
-  meetupPath: `${__dirname}/../meetups`,
+  meetupPath: `${__dirname}/../meetups`
 };
 
-EventDirCreator.setup = (githubAccessToken) => {
+EventDirCreator.setup = githubAccessToken => {
   EventDirCreator.githubAccessToken = githubAccessToken;
 };
 
@@ -27,42 +28,46 @@ EventDirCreator.getNextEventNum = () => {
   return nextEventNum;
 };
 
-EventDirCreator.createDirWithNum = (num) => {
+EventDirCreator.createDirWithNum = num => {
   fs.mkdirSync(`${EventDirCreator.meetupPath}/${num}`);
-  fs.writeFileSync(`${EventDirCreator.meetupPath}/${num}/kpt.md`, '');
+  fs.writeFileSync(`${EventDirCreator.meetupPath}/${num}/kpt.md`, "");
 
   logger.debug(`generated: ${num} meetup template`);
 };
 
-EventDirCreator.createPullRequestWithNum = (num) => {
+EventDirCreator.createPullRequestWithNum = num => {
   const branch = `vol-${num}`;
   const email = `${repo}@gmail.com`;
   const repoUrl = `https://${EventDirCreator.githubAccessToken}@github.com/${user}/${repo}`;
 
   execSync(`git checkout -b ${branch}`);
   execSync(`git add ../meetups/${num}`);
-  execSync(`GIT_AUTHOR_NAME=${repo} GIT_AUTHOR_EMAIL=${email} GIT_COMMITTER_NAME=${user} GIT_COMMITTER_EMAIL=${email} git commit -m "Create ${branch}"`);
+  execSync(
+    `GIT_AUTHOR_NAME=${repo} GIT_AUTHOR_EMAIL=${email} GIT_COMMITTER_NAME=${user} GIT_COMMITTER_EMAIL=${email} git commit -m "Create ${branch}"`
+  );
   execSync(`git push ${repoUrl} ${branch}`);
 
   const data = {
     title: `Create ${branch}`,
-    body: 'Please review pull-request',
+    body: "Please review pull-request",
     head: branch,
-    base: 'master',
+    base: "master"
   };
 
   axios({
-    method: 'post',
+    method: "post",
     url: `https://api.github.com/repos/${user}/${repo}/pulls`,
     headers: {
-      Authorization: `token ${EventDirCreator.githubAccessToken}`,
+      Authorization: `token ${EventDirCreator.githubAccessToken}`
     },
-    data,
-  }).then((response) => {
-    logger.info(response.data.html_url);
-  }).catch((err) => {
-    logger.error(err);
-  });
+    data
+  })
+    .then(response => {
+      logger.info(response.data.html_url);
+    })
+    .catch(err => {
+      logger.error(err);
+    });
 };
 
 EventDirCreator.createNextEvent = () => {
--- scripts/validate_self_introduction.js
+++ scripts/validate_self_introduction.js
@@ -1,10 +1,10 @@
-const Octokit = require('@octokit/rest');
+const Octokit = require("@octokit/rest");
 
 const octokit = new Octokit();
 const logger = console;
 
-const owner = 'shinjuku-mokumoku';
-const repo = 'shinjuku-mokumoku';
+const owner = "shinjuku-mokumoku";
+const repo = "shinjuku-mokumoku";
 
 const pullRequestNum = async () => {
   if (process.env.CIRCLE_PR_NUMBER) {
@@ -14,10 +14,10 @@ const pullRequestNum = async () => {
   const pulls = await octokit.pulls.list({
     owner,
     repo,
-    state: 'all',
+    state: "all",
     head: `${owner}:${process.env.CIRCLE_BRANCH}`,
     per_page: 1,
-    page: 1,
+    page: 1
   });
 
   const num = pulls.data[0].number;
@@ -26,35 +26,47 @@ const pullRequestNum = async () => {
   return num;
 };
 
-const meetupFiles = async (num) => {
-  logger.debug(`Pull Request is https://github.com/shinjuku-mokumoku/shinjuku-mokumoku/pull/${num}`);
+const meetupFiles = async num => {
+  logger.debug(
+    `Pull Request is https://github.com/shinjuku-mokumoku/shinjuku-mokumoku/pull/${num}`
+  );
 
   const res = await octokit.pulls.listFiles({ owner, repo, pull_number: num });
 
   // only meetups/xx/x.md. not include kpt.md
-  const fileNames = res.data.map(files => files.filename)
+  const fileNames = res.data
+    .map(files => files.filename)
     .filter(name => /meetups/.test(name))
     .filter(name => !/kpt/.test(name));
 
-  logger.debug(`fileNames: ${fileNames.join(',')}`);
+  logger.debug(`fileNames: ${fileNames.join(",")}`);
 
   return fileNames;
 };
 
-const validExtensionFile = async (fileNames) => {
+const validExtensionFile = async fileNames => {
   const fileExtensionRegexp = /meetups\/[0-9]+\/.+\.md/;
 
   return fileNames.filter(name => fileExtensionRegexp.test(name))[0];
 };
 
 const isValidFormatFile = async (prNumber, filePath) => {
-  const resPullRequest = await octokit.pulls.get({ owner, repo, pull_number: prNumber });
-  const [headOrg, headBranch] = resPullRequest.data.head.label.split(':');
+  const resPullRequest = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: prNumber
+  });
+  const [headOrg, headBranch] = resPullRequest.data.head.label.split(":");
 
   const resContent = await octokit.repos.getContents({
-    owner: headOrg, repo, path: filePath, ref: headBranch,
+    owner: headOrg,
+    repo,
+    path: filePath,
+    ref: headBranch
   });
-  const content = Buffer.from(resContent.data.content, 'base64').toString('UTF-8');
+  const content = Buffer.from(resContent.data.content, "base64").toString(
+    "UTF-8"
+  );
 
   const formatRegexp = `#.*
 ## 会社や業務で普段やっていること
@@ -69,16 +81,17 @@ const isValidFormatFile = async (prNumber, filePath) => {
 .*
 `;
 
-
   // NOTE Remove `\n` to need regexp matching
-  return RegExp(formatRegexp.replace(/\n/g, '')).test(content.replace(/\n/g, ''));
+  return RegExp(formatRegexp.replace(/\n/g, "")).test(
+    content.replace(/\n/g, "")
+  );
 };
 
 const isValid = async () => {
   const num = await pullRequestNum();
   const fileNames = await meetupFiles(num);
   if (fileNames) {
-    logger.debug('This PR does not include self introduction file.');
+    logger.debug("This PR does not include self introduction file.");
     process.exit(0);
   }
   const filePath = await validExtensionFile(fileNames);
@@ -86,13 +99,13 @@ const isValid = async () => {
   logger.debug(`validFilePath: ${filePath}`);
 
   if (!filePath) {
-    logger.debug('Invalid file extension');
+    logger.debug("Invalid file extension");
     process.exit(1);
   }
 
   const isValidFormat = await isValidFormatFile(num, filePath);
   if (!isValidFormat) {
-    logger.error('Format is Invalid. Please use meetups/template.md');
+    logger.error("Format is Invalid. Please use meetups/template.md");
     process.exit(1);
   }
 
